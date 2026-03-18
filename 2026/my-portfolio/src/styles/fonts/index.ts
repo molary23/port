93e3a28e387c87ca209dbbaeed6b9c0d21204ff9
Hassan Azeez Adeola <molary23@gmail.com>
2026-03-18 18:18:24 -0400

    PF-01: Setup Project

---
+++ 2026/my-portfolio/src/styles/fonts/index.ts
@@ -0,0 +1,43 @@
+// import localFont from "next/font/local";
+
+// export const poppins = localFont({
+//   src: [
+//     {
+//       path: "./poppins/Poppins-Regular.ttf",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./poppins/Poppins-SemiBold.ttf",
+//       weight: "600",
+//       style: "semibold",
+//     },
+//     {
+//       path: "./poppins/Poppins-Bold.ttf",
+//       weight: "700",
+//       style: "bold",
+//     },
+//   ],
+//   variable: "--font-poppins",
+// });
+
+// export const nunitoSans = localFont({
+//   src: [
+//     {
+//       path: "./nunito/NunitoSans-Regular.ttf",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "./nunito/NunitoSans-SemiBold.ttf",
+//       weight: "600",
+//       style: "semibold",
+//     },
+//     {
+//       path: "./nunito/NunitoSans-Bold.ttf",
+//       weight: "700",
+//       style: "bold",
+//     },
+//   ],
+//   variable: "--font-nunitoSans",
+// });
